0af75cefe85bcfb97142ffa2da946fafc2ffdcd9
Starlight <StarlightWT@protonmail.com>
2023-05-10 12:25:24 +0200

    Created a contact page

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ function selectYear(year) {
   element.setAttribute("class", "year active");
   lastElement = element;
 }
+
+function copy(text) {
+  navigator.clipboard.writeText(text);
+  var element = document.getElementById("copy_notif");
+  element.setAttribute("class", "copied_text_obj active");
+  setTimeout(() => {
+    var element = document.getElementById("copy_notif");
+    element.setAttribute("class", "copied_text_obj");
+  }, 3000);
+}
